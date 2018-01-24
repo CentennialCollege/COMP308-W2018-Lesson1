26241f2455aca1d843471183e41750e38eb3d38c
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-01-23 20:02:02 -0500

    added nodemon

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.get('/', (req, res) => res.send('Hello, World!'));
+
+app.get('/home', (req, res) => res.send('You are at home!'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
 /*const http = require('http');
 
